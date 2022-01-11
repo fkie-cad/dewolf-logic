49fff7905abe7b8fc9456d52d35e848220e932ce
Eva-Maria Behner <eva-maria.behner@fkie.fraunhofer.de>
2022-01-11 09:46:00 +0100

    rename file

--- simplifier/visitor/__init__.py
+++ simplifier/visitor/__init__.py
@@ -1,4 +1,4 @@
 """Module defining the Visitor patterns for WorldObjects."""
 from simplifier.visitor.print_visitor import PrintVisitor
-from simplifier.visitor.to_cnf_visitor import ToCnfVisitor, ToDnfVisitor
+from simplifier.visitor.to_normal_form_visitor import ToCnfVisitor, ToDnfVisitor
 from simplifier.visitor.visitor import WorldObjectVisitor
